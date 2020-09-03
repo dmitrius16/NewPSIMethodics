fba405befd4c97eacf8eaa9b913c95aaf92d0f56
Sysoev Dmitriy <sysoevdmitriy@gmail.com>
2020-09-03 07:13:13 +0300

    Work on process errors

--- new_PSI_project/make_psi.py
+++ new_PSI_project/make_psi.py
@@ -18,8 +18,10 @@ def init():
     
     # open svg channel Binom
     # may be check opend sess and close them via simple socket connection and 23 port    
-    binom_data.connect()    
-
+    # read csv
+    measurement.create_dict_test_points(sys.argv[1])
+    binom_data.connect()
+    
 
 
 def deinit():
