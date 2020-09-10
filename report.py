cced4385e64804e45a2a9bba62254ce981331881
Sysoev Dmitriy <sysoevdmitriy@gmail.com>
2020-09-10 12:45:09 +0300

    Change Save excel format

--- report.py
+++ report.py
@@ -4,52 +4,15 @@ import measurement
 import names_parameters as nm_par
 
 template_file_name = ".\\Template.xlsx"
-head_range = "A1:AJ1"
-result_range = "A2:AJ13"
+
+# head_range = "A1:AJ1"
+# result_range = "A2:AJ13"
+
+result_range = "A1:AJ13"
 
 cur_col_output = "A2"
 
-
 '''
-import xlwings as xw
-
-wb = xw.Book('mybook.xlxs')
-sht = wb.sheets['Sheet1']
-sht2 = wb.sheets['Sheet2']
-
-sht.range('A1:A6').api.copy
-sht2.range("A1").api.select
-sht2.api.paste
-
-wb.app.api.CutCopyMode=False
-'''
-
-# class 
-
-"""
-def create_test_report(num_point, signal_fr_csv):
-    '''
-
-    '''
-    result_file = '.\\Reports\\Report_' + time.strftime("%Y_%m_%d:%H:%M:%S",time.localtime()) + ".xlsx"
-    wb_template = xs.Book(template_file_name)
-    
-    wb = xs.Book()
-    # copy template to new excel 
-    # wb_template.sheets["Template"].Range(template_range).copy(wb.sheets["Result"].range(template_range))
-    wb_template.sheets["Template"]
-
-    '''
-    Todo: 
-    1 write copy header 
-    2 write set values (from csv)
-    3 wrute MTE meas values with ()
-    '''
-
-
-    wb.save(result_file) 
-"""
-
 def copy_head_fr_template(wb_src, wb_dest):
     src_sh = wb_src.sheets['Template']
     dst_sh = wb_dest.sheets[0]
@@ -58,13 +21,30 @@ def copy_head_fr_template(wb_src, wb_dest):
     dst_sh.range("A1").api.select
     dst_sh.api.paste
 
-def copy_result_field_fr_template(wb_src, wb_dest):
+def copy_result_field_fr_template(wb_src, wb_dest, cur_pnt):
     src_sh = wb_src.sheets['Template']
     dst_sh = wb_dest.sheets[0]
 
     src_sh.range(result_range).api.copy
     dst_sh.range(cur_col_output).api.select
     dst_sh.api.paste
+'''
+
+def copy_result_fr_template(wb_src, wb_dst, cur_pnt):
+    src_sh = wb_src.sheets['Template']
+    # wb_dst.sheets[sheet_name].activate()
+    sheet_name = get_sheet_name(cur_pnt)
+    dst_sh = wb_dst.sheets[sheet_name] 
+    # make name from cur_pnt
+    #dst_sh.sheets[current_sheet].name = get_sheet_name(cur_pnt)
+    
+    src_sh.range(result_range).api.copy
+    dst_sh.range("A1").api.select
+    dst_sh.api.paste
+
+
+def get_sheet_name(cur_pnt):
+    return "pnt #{}".format(cur_pnt)
 
 def next_output_cell(wb_result):
     global cur_col_output
@@ -80,7 +60,8 @@ def __write_meas_result(wb_result, cur_pnt, meas_result, offset):
     meas_result - instance MeasuredSignal
     offset - typle (row, column) offset
     '''
-    cur_range = wb_result.sheets[0].range(cur_col_output).offset(offset[0], offset[1])
+    sheet_name = get_sheet_name(cur_pnt)
+    cur_range = wb_result.sheets[sheet_name].range(cur_col_output).offset(offset[0], offset[1])
     cur_range.value = cur_pnt
     cur_range = cur_range.offset(0, 1)
     cur_range.value = list(meas_result.results.values())
@@ -94,7 +75,8 @@ def __write_meas_errors(wb_result, cur_pnt, meas_result, offset):
     offset - !!!! offset result must be same as in call __write_meas_result
 
     '''
-    cur_range = wb_result.sheets[0].range(cur_col_output).offset(offset[0], offset[1])
+    sheet_name = get_sheet_name(cur_pnt)
+    cur_range = wb_result.sheets[sheet_name].range(cur_col_output).offset(offset[0], offset[1])
     cur_range = cur_range.offset(1, 1)
     # very slow speed below code
     '''
@@ -146,25 +128,32 @@ def generate_report(st_pnt, end_pnt):
   
     wb_template = xs.Book(template_file_name)
     wb_result = xs.Book()
-    copy_head_fr_template(wb_template, wb_result)
+    #copy_head_fr_template(wb_template, wb_result)
 
 
     #-------------------------------------------------
     cur_pnt = st_pnt
+    cnt_pnts = 0
     while cur_pnt <= end_pnt:
         measurement.measurement_storage.calc_error(cur_pnt)
-        copy_result_field_fr_template(wb_template, wb_result)
+        #copy_result_field_fr_template(wb_template, wb_result)
+
+        wb_result.sheets.add(get_sheet_name(cur_pnt), before=wb_result.sheets[cnt_pnts])
+        # wb_result.sheets[sheet_name].activate()
+        copy_result_fr_template(wb_template, wb_result, cur_pnt)
 
         write_etalon_result(wb_result, cur_pnt)
         write_mte_result(wb_result, cur_pnt) 
         write_binom_result(wb_result, cur_pnt)
 
 
-        next_output_cell(wb_result)
+        # next_output_cell(wb_result)
         cur_pnt += 1
+        cnt_pnts += 1
+        
     #-------------------------------------------------
+    wb_result.sheets[0].activate()
     wb_result.save(res_excel_file)
-    pass
 
 
 if __name__ == '__main__':
